b9dfd5195d1f9b82c8a2e4dcd317258f72eacdbc
Delisha Cogdell <ceedeelight@yahoo.com>
2022-07-24 09:38:49 -0400

    correct spelling

--- solution93.js
+++ solution93.js
@@ -1,4 +1,4 @@
-//There are a certain number of monkeys(n) you son is too young ans has to start counting from 1.
+//There are a certain number of monkeys(n) you son is too young and has to start counting from 1.
 //Given the number(n), populate an ARRAY with all numbers up to and including that number, but exclude zero.
 //ex: input-->output 10-->[1,2,3,4,5,6,7,8,9,10]
 //1 --> [1]
