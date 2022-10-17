8c2814f0f76794718d4e4b86824277a2b4f209cf
Delisha Cogdell <ceedeelight@yahoo.com>
2022-10-17 10:38:27 -0400

    Using a Ternary

---
+++ solution90.js
@@ -0,0 +1,8 @@
+//Write a function that given a floor in the american system returns the floor in the european system.
+//With the 1st floor being replaced by the ground floor and the 13th floor being removed, the numbers move down to take their place. 
+//In case of above 13, they move down by two because there are two omitted numbers below them.
+//Basements (negatives) stay the same as the universal level.
+// Use a Tenary
+function getRealFloor(n){
+    return (n > 13 ? n -2 : n > 0 ? n -1 : n );
+}
