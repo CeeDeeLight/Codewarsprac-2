515456145ebc5240058fe13f0fb71525b751552a
Delisha Cogdell <ceedeelight@yahoo.com>
2022-10-23 18:57:50 -0400

    create funct that returns CSV representation of a 2-dimens. numeric array.

---
+++ solution96.js
@@ -0,0 +1,16 @@
+//Create function that returns the CSV representation of a two-dimenstional numeric array.
+//example input:
+//   [[ 0, 1, 2, 3, 4 ],
+//   [ 10,11,12,13,14 ],
+//   [ 20,21,22,23,24 ],
+//   [ 30,31,32,33,34 ]] 
+   
+//output:
+//    '0,1,2,3,4\n'
+//   +'10,11,12,13,14\n'
+//   +'20,21,22,23,24\n'
+//   +'30,31,32,33,34'
+
+function toCsvText(array) {
+   return array.join ('\n');
+ }
