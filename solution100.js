7b5d784428163704565e954dfd9b5b21329b05fd
Delisha Cogdell <ceedeelight@yahoo.com>
2022-10-27 18:04:13 -0400

    Find smallest element in this array of integers

---
+++ solution100.js
@@ -0,0 +1,15 @@
+/*Given an array of integers , Find the Nth smallest element in this array of integers.
+Notes
+Array/list size is at least 3 .
+Array/list's numbers could be a mixture of positives , negatives and zeros .
+Repetition in array/list's numbers could occur , so don't Remove Duplications .
+nthSmallest({3,1,2} ,2) ==> return (2) 
+nthSmallest({15,20,7,10,4,3} ,3) ==> return (7) 
+nthSmallest({15,20,7,10,4,3} ,3) ==> return (7) 
+nthSmallest({177,225,243,-169,-12,-5,2,92} ,5) ==> return (92) 
+*sort the array and then grab the nth value. !remember how arrays are indexed
+*/
+
+function nthSmallest(arr, pos){
+    return arr.sort((a,b)=>a-b)[pos-1]
+}
