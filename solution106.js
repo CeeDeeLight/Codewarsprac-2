5634a828bc59a529fb1e3ec275f0fd6dd2d907a4
Delisha Cogdell <ceedeelight@yahoo.com>
2022-11-02 17:35:24 -0400

    Filter coffee

---
+++ solution106.js
@@ -0,0 +1,5 @@
+/* The first argument to your search function will be a number which represents your budget.The second argument will be an array of coffee bean prices.
+Your 'search' function should return the stores that sell coffee within your budget. The search function should return a
+string of prices for the coffees beans you can afford. The prices in this string are to be sorted in ascending order. */
+
+let search = (budget, prices) => prices.filter (p => p <= budget).sort((a,b) => a-b).join(',');
