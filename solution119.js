c8093bc810f9d308f2460d6f962881b514922f63
Delisha Cogdell <ceedeelight@yahoo.com>
2022-11-15 07:43:48 -0500

    Write a reverse function which reverses a list.

---
+++ solution119.js
@@ -0,0 +1,8 @@
+/* Write a function reverse which reverses a list (or in clojure's case, any list-like data structure)*/
+reverse = function(array) {
+    let result = []
+    for (let i = array.length -1; i >= 0; i--) {
+      result.push(array[i])
+    }
+    return result;
+  }
