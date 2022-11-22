d1912dca36f9638ef71067995df2a2aa6177f4fd
Delisha Cogdell <ceedeelight@yahoo.com>
2022-11-22 08:02:04 -0500

    Javscript practice

---
+++ solution126.js
@@ -0,0 +1,21 @@
+/* This is not a codewars problem. This is just me doing a java exercise.
+Define an addFavoriteBook(..) function that receives a
+single parameter, called bookname
+If the provided bookName string does NOT have the word "Great" in it, add it to the favoriteBooks array. */
+
+function addFavoriteBook (bookName) {
+    if (!bookName.includes('Great')) {
+        favoriteBooks.push(bookName);
+    }
+ }
+ 
+ let favoriteBooks = [];
+ 
+ addFavoriteBook('A Song of Ice and Fire')
+ addFavoriteBook('The Great Gatsby');
+ addFavoriteBook('Crime & Punishment');
+ addFavoriteBook('Great Expectations');
+ addFavoriteBook('You Don"t Know JS');
+ addFavoriteBook('Eloquent Javascript');
+ 
+ console.log(favoriteBooks);
