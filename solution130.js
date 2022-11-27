b6928f71ddd5cb043971ceca7e557b453715ef07
Delisha Cogdell <ceedeelight@yahoo.com>
2022-11-26 21:03:46 -0500

    write func called _ that always returns 5

---
+++ solution130.js
@@ -0,0 +1,5 @@
+/*Write a function/proc/lambda called _ that always returns 5. 
+Sounds easy right? Just bear in mind that you can only use the following characters:
+~!@#$%^&*()-_+=[]\{}|:;"'<,>.?/`*/
+
+_=()=>(''=='')+(''=='')+(''=='')+(''=='')+(''=='')
