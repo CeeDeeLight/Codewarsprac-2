81e077d28f9104e29921c2c66fdc7997fba44cfa
Delisha Cogdell <ceedeelight@yahoo.com>
2022-11-30 09:19:44 -0500

    London City Hacker

---
+++ solution134.js
@@ -0,0 +1,21 @@
+/*The sequence will contain bus numbers and TFL tube names as strings e.g.
+['Northern', 'Central', 243, 1, 'Victoria'] 
+Journeys will always only contain a combination of tube names and bus numbers. 
+Each tube journey costs £2.40 and each bus journey costs £1.50. If there are 2 or more adjacent bus journeys, the bus fare is 
+capped for sets of two adjacent buses and calculated as one bus fare for each set.
+For example: 'Piccadilly', 56, 93, 243, 20, 14 -> "£6.90"
+Your task is to calculate the total cost of the journey and return the cost rounded 
+to 2 decimal places in the format (where x is a number): £x.xx */
+
+function londonCityHacker(journey) {
+    let sum = 0;
+  
+    for (let i = 0; i < journey.length; i++) {
+        if (typeof journey[i] === "string") sum += 2.40;
+            else {
+              sum += 1.50;
+        if (typeof journey[i + 1] === "number") i++;
+    }
+  }
+  return `£${sum.toFixed(2)}`;
+  }
