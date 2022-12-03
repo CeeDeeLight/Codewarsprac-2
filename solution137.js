bebbfa4420618bcf59b5a4d62fd78a4ead45ea0d
Delisha Cogdell <ceedeelight@yahoo.com>
2022-12-03 11:26:09 -0500

    Leap Years

---
+++ solution137.js
@@ -0,0 +1,7 @@
+/* Leap Years determine, whether a given year is a leap year or not. In case you don't know the rules, here they are:
+years divisible by 4 are leap years
+but years divisible by 100 are not leap years
+but years divisible by 400 are leap years*/
+function isLeapYear(year) {
+    return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+  }
