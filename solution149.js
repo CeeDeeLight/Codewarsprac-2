0b1d3eb6c7b8ecaee4f6d62e0bec2c4783e4477b
Delisha Cogdell <ceedeelight@yahoo.com>
2022-12-15 20:26:55 -0500

    Power of i

---
+++ solution149.js
@@ -0,0 +1,8 @@
+/*Powers of i
+i is the imaginary unit, it is defined by i²=−1i² = -1i²=−1, therefore it is a solution to 
+x²+1=0x² + 1 = 0x²+1=0. Complete the function pofi that returns iii to the power 
+of a given non-negative integer in its simplest form, as a string (answer may contain iii). */
+
+function pofi(n) {
+    return ["1", "i","-1", "-i"] [n % 4];
+}
