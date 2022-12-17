5a52c7f23057bcf031fa4f6471622dbb47cc0a44
Delisha Cogdell <ceedeelight@yahoo.com>
2022-12-17 15:24:33 -0500

    Exclusinve 'or' (xor) Logical Operator

---
+++ solution151.js
@@ -0,0 +1,7 @@
+/* Exclusinve "or" (xor) Logical Operator  your task is to define a function xor(a, b) where a and b are 
+the two expressions to be evaluated. Your xor function should have the behaviour described above, 
+returning true if exactly one of the two expressions evaluate to true, false otherwise.*/
+function xor(a, b) {
+    if (a !== b) return true;
+     else return false;
+  }
