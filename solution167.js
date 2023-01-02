8d7722d8f74c6de5f8c56f563d983244b33b0aab
Delisha Cogdell <ceedeelight@yahoo.com>
2023-01-02 11:47:26 -0500

    Array checks if value and returns T or F

---
+++ solution167.js
@@ -0,0 +1,8 @@
+//You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+//The array can contain numbers or strings. X can be either.
+//Return true if the array contains the value, false if not.0.
+
+function check(a, x) {
+    // your code here
+  return a.includes(x);
+  }
