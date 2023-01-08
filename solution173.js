7a203a9a2308c2032d21bb34620695f3424467a8
Delisha Cogdell <ceedeelight@yahoo.com>
2023-01-08 17:44:01 -0500

    ReturnToSanity/ReturnAnObj

---
+++ solution173.js
@@ -0,0 +1,14 @@
+//Return to Sanity: function should return an object, but it's not doing what's intended. What's wrong?
+
+/* function mystery() {
+    var results =
+      {sanity: 'Hello'};
+    return
+      results;
+  } */
+
+  //Correct Arrangement:
+  function mystery() {
+    let results =  {sanity: 'Hello'};
+    return results;
+  }
