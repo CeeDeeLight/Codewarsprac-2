53b3ecdd829ee2aa99a0a611dcff5c31d4a58138
Delisha Cogdell <ceedeelight@yahoo.com>
2023-01-11 08:19:20 -0500

    Point Reflection

---
+++ solution176.js
@@ -0,0 +1,13 @@
+/* "Point reflection" or "point symmetry" is a basic concept in geometry where a given point, P, at a given position relative 
+to a mid-point, Q has a corresponding point, P1, which is the same distance from Q but in the opposite direction.Task
+Given two points P and Q, output the symmetric point of point P about Q. Each argument is a two-element array of integers 
+representing the point's X and Y coordinates. Output should be in the same format, giving the X and Y coordinates of point P1. 
+You do not have to validate the input.  */
+
+function symmetricPoint(p, q) {
+    const [xp, yp] = p;
+    const [xq, yq] = q;
+    const xDist = (xp - xq) * -1;
+    const yDist = (yp - yq) * -1;
+    return [xq + xDist, yq + yDist]
+  }
