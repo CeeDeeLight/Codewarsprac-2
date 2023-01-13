8fbe438ef7ecec6fcb9997d654c4a0cecc8e3977
Delisha Cogdell <ceedeelight@yahoo.com>
2023-01-13 10:52:55 -0500

    Area or Perimeter?

---
+++ solution178.js
@@ -0,0 +1,10 @@
+/* Area or Perimeter You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
+If it is a square, return its area. If it is a rectangle, return its perimeter.
+Example(Input1, Input2 --> Output):
+6, 10 --> 32
+3, 3 --> 9  */
+
+const areaOrPerimeter = function(l , w) {
+    if (l ===w) return l * w;
+    else return 2 * l + 2 * w;
+  };
