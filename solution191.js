95a9a2411cb356f021ac5bf6181ec54144627ca2
Delisha Cogdell <ceedeelight@yahoo.com>
2023-01-26 08:31:52 -0500

    Cats on a Shelf

---
+++ solution191.js
@@ -0,0 +1,13 @@
+/* An infinite number of shelves are arranged one above the other in a staggered fashion.
+The cat can jump either one or three shelves at a time: from shelf i to shelf i+1 or i+3 (the cat cannot climb on the shelf directly above its head), 
+according to the illustration:
+Input Start and finish shelf numbers (always positive integers, finish no smaller than start)
+Task
+Find the minimum number of jumps to go from start to finish
+Example
+Start 1, finish 5, then answer is 2 (1 => 4 => 5 or 1 => 2 => 5) */
+
+function solution(start, finish) 
+{
+  return Math.floor((finish - start) / 3) + (finish - start) % 3;
+}
