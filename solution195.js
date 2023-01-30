9f0b259fde06ad82b6c534cba17d3e1de79bb178
Delisha Cogdell <ceedeelight@yahoo.com>
2023-01-30 14:29:29 -0500

    convert boolean value into string representation

---
+++ solution195.js
@@ -0,0 +1,5 @@
+/* Implement a function which convert the given boolean value into its string representation. 
+https://www.codewars.com/kata/551b4501ac0447318f0009cd */
+function booleanToString(b) {
+    return b ? 'true' : 'false';
+  }
