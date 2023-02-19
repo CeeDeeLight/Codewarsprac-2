b96fa8a41175d33d7515cce370a7bf992da574fb
Delisha Cogdell <ceedeelight@yahoo.com>
2023-02-19 11:56:34 -0500

    text update

--- solution215.js
+++ solution215.js
@@ -1,5 +1,5 @@
 /*We have implemented a function wrap(value) that takes a value of arbitrary type and wraps it in a new JavaScript Object
-or Python Dict setting the 'value' key on the new Object or Dict to the passed-in value.*/
+setting the 'value' key on the new Object to the passed-in value.*/
 
 function wrap(value) {
     return {  
