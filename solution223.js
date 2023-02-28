8828b02fc7ff4a278ec34157a47d0c884ddfe929
Delisha Cogdell <ceedeelight@yahoo.com>
2023-02-27 19:56:40 -0500

    Debug the code-JS

---
+++ solution223.js
@@ -0,0 +1,12 @@
+//Debug the code
+/*The code is not working as designed. Please fix the code so that it behaves as specified.
+function wrap(value) {
+  return
+  {
+    value:value 
+  };
+} */
+
+function wrap(value) {
+    return {value};
+  }
