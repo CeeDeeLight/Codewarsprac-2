8d50d670a0944716069f5d060775a5e362c697db
Delisha Cogdell <ceedeelight@yahoo.com>
2023-03-11 13:11:23 -0500

    Boolean & if/else

---
+++ solution235.js
@@ -0,0 +1,11 @@
+/* Training JS #6: Basic data types--Boolean and conditional statements if..else
+https://www.codewars.com/kata/571f832f07363d295d001ba8
+Question:
+function trueOrFalse(val){
+  if ()    return ?;             
+  else     return ?;
+} */
+
+function trueOrFalse(val){
+    return val ? 'true' : 'false';
+ }
