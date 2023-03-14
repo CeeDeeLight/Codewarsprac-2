63db843124e407e2d2a326a3f628d10a130b264d
Delisha Cogdell <ceedeelight@yahoo.com>
2023-03-14 08:32:41 -0400

    How many bullets for the Dragons.

---
+++ solution238.js
@@ -0,0 +1,10 @@
+//Each dragon takes 
+//2 bullets to be defeated, our hero has no idea how many bullets he should carry. 
+//Assuming he's gonna grab a specific given number of bullets and move forward to 
+//fight another specific given number of dragons, will he survive?
+//Return True if yes, False otherwise :)
+
+function hero(bullets, dragons){
+    //Get Coding!
+    return bullets >= 2 * dragons;
+    }
