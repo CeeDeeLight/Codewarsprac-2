3bf892431e035fd1740869a846f9a96c0b4d0afc
Delisha Cogdell <ceedeelight@yahoo.com>
2023-03-17 15:03:41 -0400

    This method uses the js require() function

---
+++ solution241.js
@@ -0,0 +1,8 @@
+/* 
+function keepOrder(array, value){
+    //your code here
+}  */
+
+//this is using the js require() function https://www.freecodecamp.org/news/how-to-use-the-javascript-require-function/
+
+const keepOrder = require('lodash').sortedIndex;
